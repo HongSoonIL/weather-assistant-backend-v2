543d41363f3eb835815fa80ed7d875b72651123b
nayoniee <nayeonii720@gmail.com>
2025-06-23 01:00:40 +0900

    .

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -3,7 +3,7 @@ const admin = require('firebase-admin');
 const path = require('path');
 
 // 🔐 JSON 키 경로 - 프로젝트 루트에 위치한 JSON 파일
-const serviceAccount = require('/etc/secrets/lumeeweatherapp-firebase-adminsdk-fbsvc-ffbb9087de.json');
+const serviceAccount = require('./lumeeweatherapp-firebase-adminsdk-fbsvc-ffbb9087de.json');  // /etc/secrets/lumeeweatherapp-firebase-adminsdk-fbsvc-ffbb9087de.json
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
